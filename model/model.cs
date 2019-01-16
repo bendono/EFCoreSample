a1035ae143a738b79b3ca53c2e0c1694e18bf53f
Ben Monroe <bendono@gmail.com>
2019-01-16 21:49:21 +0900

    Add project files.

---
+++ model/model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Foo
+{
+    public class Foo
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+
+    public class Bar
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+
+    public class FooContext : DbContext
+    {
+        public DbSet<Foo> Foos { get; set; }
+        public DbSet<Bar> Bars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=foo.db");
+        }
+    }
+}
